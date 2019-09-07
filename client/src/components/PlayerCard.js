26c93b733bbf00f171492e66aa9dc4b2fe348f26
Min-Huang <minh.huang96@gmail.com>
2019-09-07 01:47:33 -0700

    created app and got data

---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PlayerCard = props => {
+  console.log(props.player);
+
+  return (
+    <div className="PlayerCardContainer">
+      
+    </div>
+  );
+};
+
+export default PlayerCard;
